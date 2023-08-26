0b92b95b22e3c6bca6cbfda37e0d4dc67c796571
Idris Haider <idrishaider987@gmail.com>
2023-08-26 20:38:49 +0530

    Delete tmc directory

--- tmc/src/components/Head.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Helmet from "react-helmet";
-export default function Head({title=""}) {
-  return (
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
-  );
-}
--- tmc/src/pages/Other/TMCMarket.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TMCMarket() {
-  return (
-    <div>TMCMarket</div>
-  )
-}
